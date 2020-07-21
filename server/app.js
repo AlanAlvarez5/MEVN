9828018f997f97a49367e605219e740d506fd59e
Alan <alanalvarez98.aa@gmail.com>
2020-07-21 16:50:28 -0500

    Configuración de servidor y middelwares

--- server/app.js
+++ server/app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res){
-     res.send('Hola Mundo')
-})
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const history = require('connect-history-api-fallback');
 
-app.listen(3000, function(){
-     console.log('Escuchando en puerto 3000')
+// Middelware
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+
+// Routes
+// app.get('/', function(req, res){
+//      res.send('Hola Mundo')
+// })
+
+// Vues Middelware
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('port', process.env.PORT || 3000)
+
+app.listen(app.get('port'), function(){
+     console.log('Escuchando en puerto ', app.get('port'))
 })
